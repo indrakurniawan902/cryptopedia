4d8fd3e81b353bada4247b65970a2c04c6c2af4a
indrakurniawan <indrakurniawan902@gmail.com>
2022-10-16 21:38:35 +0700

    splash screen
    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cryptopedia/view/homepage.dart';
+import 'package:cryptopedia/screen/on_boarding/on_boarding.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Homepage(),
+      home: const OnBoarding(),
     );
   }
 }
