74b79f79f71624d21827a66fb62599fa8f0b286d
Rian Febriansyah <rianfebriansyah22@gmail.com>
2022-10-28 21:47:16 +0700

    feat: adjust api endpoint

--- lib/utils/constant/api_constant.dart
+++ lib/utils/constant/api_constant.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   static String baseUrl = 'https://ternakcoin.vercel.app/api';
-  static String postEndpoint = '/post';
+  static String postEndpoint = '/v2/getPost';
   static String checkRegister = '/isRegistered';
   static String register = '/registerUser';
-  static String addPost = '/addPost';
+  static String addPost = '/v2/addPost';
 }
